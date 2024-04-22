27e766ad830ca320d5207f4656f35dc8f13934a2
George Vitaly Avram <91331028+georgeavram@users.noreply.github.com>
2024-04-22 11:07:05 +0100

    Add files via upload

---
+++ Old Script Versions/script v01.py
@@ -0,0 +1,78 @@
+import os
+import time
+import paho.mqtt.client as mqtt
+import ssl
+import json
+import _thread
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to AWS IOT: " + str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.tls_set(ca_certs='./AmazonRootCA1.pem', certfile='./certificate.pem.crt', keyfile='./private.pem.key', tls_version=ssl.PROTOCOL_SSLv23)
+client.tls_insecure_set(False)
+client.connect("a1dj36pl4u3m7q-ats.iot.us-east-1.amazonaws.com", 8883, 60)
+
+
+def read_temperature():
+    try:
+        # Find the DS18B20 sensor device file
+        sensor_file = ''
+        for device_folder in os.listdir('/sys/bus/w1/devices'):
+            if device_folder.startswith('28-'):
+                sensor_file = os.path.join('/sys/bus/w1/devices', device_folder, 'w1_slave')
+
+        if not sensor_file:
+            raise FileNotFoundError("DS18B20 sensor not found")
+
+        # Read the raw temperature data from the sensor
+        with open(sensor_file, 'r') as file:
+            lines = file.readlines()
+
+        # Parse the temperature value
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temperature_str = lines[1][equals_pos + 2:]
+            temperature_celsius = float(temperature_str) / 1000.0
+            return temperature_celsius
+        else:
+            raise ValueError("Error parsing temperature data")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+def get_time():
+        current_time = time.time()
+        formatted_time = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime(current_time))
+        return formatted_time
+
+
+def publishData(txt):
+    print(txt)
+    while (True):
+        temperature = read_temperature()
+        current_time = get_time()
+        if temperature is not None:
+            Temp = int(float(temperature))
+        else:
+            Temp = ("Failed to read temperature data")
+        DeviceName = "LivingRoom"
+        print(Temp)
+        print(DeviceName)
+        print(current_time)
+        client.publish("raspi/tempsense", payload=json.dumps({"DeviceName": DeviceName, "Temp": Temp, "Time": current_time }), qos=0, retain=False)
+
+
+        time.sleep(5)
+_thread.start_new_thread(publishData,("Spin-up new Thread...",))
+
+client.loop_forever()
+
+
+
---
+++ Old Script Versions/script v03.py
@@ -0,0 +1,52 @@
+import os
+import time
+import paho.mqtt.client as mqtt
+import ssl
+import json
+import _thread
+import dht11
+import RPi.GPIO as GPIO
+
+# Starts GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# read data using pin 17 for DHT11 sensor
+instance = dht11.DHT11(pin=17)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to AWS IoT: " + str(rc))
+
+# create secure communication
+client = mqtt.Client()
+client.on_connect = on_connect
+client.tls_set(ca_certs='./AmazonRootCA1.pem', certfile='./certificate.pem.crt', keyfile='./private.pem.key', tls_version=ssl.PROTOCOL_SSLv23)
+client.tls_insecure_set(False)
+client.connect("a1a1qi25dvl0jn-ats.iot.us-east-1.amazonaws.com", 8883, 60)
+
+# Publish data to MQTT topic
+def publishData(txt):
+    print(txt)
+    while True:
+        try:
+            result = instance.read()
+            if result.is_valid():
+                temperature = int(result.temperature)
+                humidity = int(result.humidity)
+                DeviceName = "LivingRoom"
+
+                print("Temperature:", temperature, "°C")
+                print("Humidity:", humidity, "%")
+                print("DeviceName:", DeviceName)
+                print("------------------------")
+
+                client.publish("raspi/tempsense", payload=json.dumps({"DeviceName": DeviceName, "Temperature": temperature, "Humidity": humidity}), qos=1, retain=False)
+            else:
+                raise ValueError("Failed to read temperature and humidity data from DHT11 sensor")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+        time.sleep(5)
+_thread.start_new_thread(publishData, ("Spin-up new Thread...",))
+client.loop_forever()
